8c5aaf722aa26c7fc172856bc5ef972bc06b9d6d
Larissa <larisssa_rga@hotmail.com>
2022-08-18 19:24:45 -0300

    Exercicio js até numero 5

--- fundamentos/bloco-4-introducao-js-e-logica-de-programacao/dia-1-introducao-js/scripts.js
+++ fundamentos/bloco-4-introducao-js-e-logica-de-programacao/dia-1-introducao-js/scripts.js
@@ -1,3 +1,4 @@
+//1 
 const myName = "Larissa";
 const birthCity = "Belo Horizonte";
 let birthYear = 1996;
@@ -5,3 +6,62 @@ birthYear = 2030;
 console.log(birthYear);
 console.log(myName);
 console.log(birthCity);
+
+//2
+const base = 5;
+let height = 8;
+const area = base * height;
+const perimeter = (base * 2) + (height * 2);
+console.log(area);
+console.log(perimeter);
+
+//3
+let notaPessoa = 82;
+if (notaPessoa >= 80) {
+    console.log("Parabéns, você foi aprovado");
+} else if (notaPessoa < 80 && notaPessoa >= 60) {
+    console.log("Você está na nossa lista de espera");
+} else if (notaPessoa < 60) {
+    console.log("Você foi reprovado");
+}
+
+//4
+const currentHour = 12;
+let message = "";
+if (currentHour >= 22) {
+    message = "Não deveríamos comer nada, é hora de dormir";
+} else if (currentHour >= 18 && currentHour < 22) {
+    message = "Rango da noite, vamos jantar :D";
+} else if (currentHour >= 14 && currentHour < 18) {
+    message = "Vamos fazer um bolo pro café da tarde?"
+} else if (currentHour >= 11 && currentHour < 14) {
+    message = "Hora do almoço!!!!"
+} else if (currentHour >= 4 && currentHour < 11) {
+    message = "Hmmm, cheiro de café recém passado"
+}
+
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
+
+//5
+let weekDay = "quarta-feira";
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+    console.log("Oba, mais um dia de aprendizado na trybe >:D");
+} else {
+    console.log("FINALMENTE, descanso merecido UwU");
+}
+
+//6
+let pessoaCandidata = 'aprovada'
+switch (pessoaCandidata) {
+    case "aprovada":
+        console.log("Parabéns, você foi aprovado");
+        break;
+    case "lista":
+        console.log("Você está na nossa lista de espera");
+        break;
+    case  "reprovada":
+        console.log("Você foi reprovado");
+}
--- fundamentos/bloco-4-introducao-js-e-logica-de-programacao/dia-1-introducao-js/scripts2.js
+++ fundamentos/bloco-4-introducao-js-e-logica-de-programacao/dia-1-introducao-js/scripts2.js
@@ -1,6 +1,57 @@
-const base = 5;
-let height = 8;
-const area = base * height;
-const perimeter = (base * 2) + (height * 2);
-console.log(area);
-console.log(perimeter);
+// Exercícios
+// 1 - Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+
+const a = 5;
+const b = 10;
+console.log(a +b) //Adição
+console.log(a - b) //Subtração
+console.log(a * b) //Multiplicação
+console.log(a / b) //Divisão
+console.log(a % b) //Módulo 
+
+// 2 - Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+
+const num1 = 4;
+const num2 = 12;
+if (num1 > num2) {
+    console.log("Num1 é maior que Num2");
+} else {
+    console.log("Nun2 é maior que Nun1");
+}
+// 3 - Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+
+const num1 = 10;
+const num2 = 20;
+const num3 = 15;
+if(num1 > num2 && num1 > num3) {
+    console.log('num1 é maior');
+} else if(num2 > num1 && num2 > num3) {
+    console.log('num2 é maior');
+} else {
+    console.log('num3 é maior');
+}
+// 4 - Faça um programa que, dado um valor recebido como parâmetro, retorne "positive" se esse valor for positivo, "negative" se for negativo, e caso não seja nem positivo e nem negativo retorne "zero".
+const precisoDePraia = 'positivo'
+switch(precisoDePraia) {
+    case "positivo":
+        console.log('positive');
+        break;
+    case "negativo":
+        console.log('negative');
+        break;
+    default:
+        console.log('zero');
+}
+
+//5 - Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+let angulo1 = 60;
+let angulo2 = 180;
+let angulo3 = 20;
+let somaAngulos = angulo1 + angulo2 + angulo3;
+if(somaAngulos === 180) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+
